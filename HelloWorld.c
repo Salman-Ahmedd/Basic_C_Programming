08e4eca26338866b2d087b5b70f4ccb5634b023b
Salman Ahmed <salman.metro.uni@gmail.com>
2022-10-06 21:49:16 +0600

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+#include<stdio.h>//its Header File
+
+int main()
+{
+    printf("Hello World !");
+    //in c programming print_function is use for print anything...
+
+    return 0;
+}
---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>//its Header File
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World !";
+    //in c programming cout is use for print anything...
+
+    return 0;
+}
